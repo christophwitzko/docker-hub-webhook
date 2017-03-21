e39610dcfba2d6956d9b6f5cf65fd2b53fe6ccc8
Christoph Witzko <github@christophwitzko.com>
2017-03-21 19:05:42 +0100

    feat: update template for zero downtime deployments

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 docker pull {{.RepoName}}:{{.Tag}}
+docker run -d --name {{.Name}}-backup {{.Params}} --restart=always -e VIRTUAL_HOST={{.Vhost}} -e LETSENCRYPT_HOST={{.Vhost}} {{.RepoName}}:{{.Tag}}
+sleep 5
 docker stop {{.Name}}
 docker rm {{.Name}}
-docker run -d --name {{.Name}} {{.Params}} {{.RepoName}}:{{.Tag}}
+docker run -d --name {{.Name}} {{.Params}} --restart=always -e VIRTUAL_HOST={{.Vhost}} -e LETSENCRYPT_HOST={{.Vhost}} {{.RepoName}}:{{.Tag}}
+sleep 5
+docker stop {{.Name}}-backup
+docker rm {{.Name}}-backup
 docker rmi $(docker images -q -f dangling=true)
 exit 0
